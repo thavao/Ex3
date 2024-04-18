4af5e693fd409d00709c962a7f98df2b2091d0f0
thavao <othavioact@gmail.com>
2024-04-18 16:14:22 -0300

    Add project files.

---
+++ Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿//3 - faça um programa que leia a idade de uma pessoa e informe se ela é maior ou menor de idade
+
+int idade;
+
+Console.Write("Digite a idade: ");
+idade = int.Parse(Console.ReadLine());
+
+if (idade < 18)
+    Console.WriteLine("Menor de idade");
+
+else 
+    Console.WriteLine("Maior de idade");
